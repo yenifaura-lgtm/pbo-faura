cd87a47144eebc0c68ef7aaf098130455f0bd8c7
yenifaura-lgtm <yenifaura@gmail.com>
2026-04-29 22:07:29 +0700

    updatebentuk5

---
+++ minggu6-selfiyenifaura/minggu6_selfiyenifaura/src/bentuk5.java
@@ -0,0 +1,16 @@
+public class bentuk5 {
+   public static void main(String[] args) {
+        int n = 5;
+         for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) System.out.print("  ");
+            for (int j = 1; j <= i; j++) System.out.print("* ");
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) System.out.print("  ");
+            for (int j = 1; j <= i; j++) System.out.print("* ");
+            System.out.println();
+        }
+        System.out.println();
+   }
+}
